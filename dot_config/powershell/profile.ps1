96291ed58f7a6fd0e7ccc8ceda7315c3799f896e
Guozhen Li <ligzligz@gmail.com>
2025-12-13 02:48:34 -0800

    powershell profile

---
+++ dot_config/powershell/profile.ps1
@@ -0,0 +1,30 @@
+$os = Get-CimInstance -ClassName Win32_OperatingSystem
+Write-Host "Operating system: $($os.OSArchitecture) $($os.Caption) version $($os.Version)"
+Write-Host "PowerShell version: $($PSVersionTable.PSVersion)"
+
+Push-Location $PSScriptRoot
+
+Get-ChildItem ps*.ps1 | ForEach-Object {. $_.FullName}
+
+function prompt {
+    $uiTitle = $PWD | Convert-Path | Split-Path -Leaf
+    $Host.UI.RawUI.WindowTitle = $uiTitle
+    Write-Host "`n$env:USERNAME" -ForegroundColor Green -NoNewline
+    if (Test-Administrator) {
+        Write-Host " as " -NoNewline
+        Write-Host "Administrator" -ForegroundColor Red -NoNewline
+        $Host.UI.RawUI.WindowTitle = $uiTitle + " (Administrator)"
+    }
+    Write-Host " at " -NoNewline
+    Write-Host $env:COMPUTERNAME -ForegroundColor Magenta -NoNewline
+    Write-Host " in " -NoNewline
+    Write-Host $ExecutionContext.SessionState.Path.CurrentLocation -ForegroundColor Cyan -NoNewline
+    $branch = try { git rev-parse --abbrev-ref HEAD 2>$null} catch {$null}
+    if ($branch){
+        Write-Host " on " -NoNewline
+        Write-Host $branch -ForegroundColor Yellow -NoNewline
+    }
+    return "`nPS $('>' * ($NestedPromptLevel + 1)) "
+}
+
+Pop-Location
---
+++ dot_config/powershell/psaliases.ps1
@@ -0,0 +1,7 @@
+Set-Alias vs devenv
+Set-Alias g git
+Set-Alias l Get-ChildItem
+Set-Alias c code
+Set-Alias cmoi chezmoi
+Set-Alias vm vim
+Set-Alias nvm nvim
